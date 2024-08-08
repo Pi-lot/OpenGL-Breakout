f2094f894e5b67648f06ce0210a29b52de3193a5
Bryce Tuton <brycey@tuton.org>
2024-08-08 21:14:14 +1000

    Translated over C++ Files and reformatted existing classes.

--- OpenGL Breakout/Resources/ResourceManager.cs
+++ OpenGL Breakout/Resources/ResourceManager.cs
@@ -2,15 +2,12 @@
 using OpenTK.Graphics.OpenGL4;
 using StbImageSharp;
 
-namespace OpenGL_Breakout.Resources
-{
-    internal static class ResourceManager
-    {
+namespace OpenGL_Breakout.Resources {
+    internal static class ResourceManager {
         public static Dictionary<string, Shader> Shaders = new();
         public static Dictionary<string, Texture2D> Textures = new();
 
-        public static Shader LoadShader(string vShaderFile, string fShaderFile, string? gShaderFile, string name)
-        {
+        public static Shader LoadShader(string vShaderFile, string fShaderFile, string? gShaderFile, string name) {
             if (Shaders.ContainsKey(name))
                 Shaders[name] = LoadShaderFromFile(vShaderFile, fShaderFile, gShaderFile);
             else
@@ -18,13 +15,11 @@ namespace OpenGL_Breakout.Resources
             return Shaders[name];
         }
 
-        public static Shader GetShader(string name)
-        {
+        public static Shader GetShader(string name) {
             return Shaders[name];
         }
 
-        public static Texture2D LoadTexture(string file, bool alpha, string name)
-        {
+        public static Texture2D LoadTexture(string file, bool alpha, string name) {
             if (Textures.ContainsKey(name))
                 Textures[name] = LoadTextureFromFile(file, alpha);
             else
@@ -32,13 +27,11 @@ namespace OpenGL_Breakout.Resources
             return Textures[name];
         }
 
-        public static Texture2D GetTexture(string name)
-        {
+        public static Texture2D GetTexture(string name) {
             return Textures[name];
         }
 
-        public static void Clear()
-        {
+        public static void Clear() {
             foreach (var shader in Shaders.Values)
                 shader.Dispose();
             foreach (var texture in Textures.Values)
@@ -47,20 +40,16 @@ namespace OpenGL_Breakout.Resources
             Textures.Clear();
         }
 
-        private static Shader LoadShaderFromFile(string vShaderFile, string fShaderFile, string? gShaderFile = null)
-        {
+        private static Shader LoadShaderFromFile(string vShaderFile, string fShaderFile, string? gShaderFile = null) {
             string vertexCode = string.Empty, fragmentCode = string.Empty;
             string? geometryCode = null;
 
-            try
-            {
+            try {
                 vertexCode = File.ReadAllText(vShaderFile);
                 fragmentCode = File.ReadAllText(fShaderFile);
                 if (gShaderFile != null)
                     geometryCode = File.ReadAllText(gShaderFile);
-            }
-            catch
-            {
+            } catch {
                 Console.WriteLine("ERROR: Failed to read shader files");
             }
 
@@ -69,18 +58,15 @@ namespace OpenGL_Breakout.Resources
             return shader;
         }
 
-        private static Texture2D LoadTextureFromFile(string file, bool alpha)
-        {
+        private static Texture2D LoadTextureFromFile(string file, bool alpha) {
             Texture2D texture = new();
             ColorComponents colourComponents;
 
-            if (alpha)
-            {
+            if (alpha) {
                 texture.Image_Format = PixelFormat.Rgba;
                 texture.Internal_Format = PixelInternalFormat.Rgba;
                 colourComponents = ColorComponents.RedGreenBlueAlpha;
-            }
-            else
+            } else
                 colourComponents = ColorComponents.RedGreenBlue;
 
             StbImage.stbi_set_flip_vertically_on_load(1);
--- OpenGL Breakout/Resources/Shader.cs
+++ OpenGL Breakout/Resources/Shader.cs
@@ -1,55 +1,43 @@
 ﻿using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
 
-namespace OpenGL_Breakout.Resources
-{
-    internal class Shader : IDisposable
-    {
+namespace OpenGL_Breakout.Resources {
+    internal class Shader : IDisposable {
         private bool disposedValue = false;
 
         public int ID { get; private set; }
 
-        public Shader()
-        {
+        public Shader() {
 
         }
 
-        public void Use()
-        {
+        public void Use() {
             GL.UseProgram(ID);
         }
 
-        public void Compile(string vertexSource, string fragmentSource, string? geometrySource = null)
-        {
+        public void Compile(string vertexSource, string fragmentSource, string? geometrySource = null) {
             int sVertex, sFragment;
             int? gShader = null;
 
             sVertex = GL.CreateShader(ShaderType.VertexShader);
             GL.ShaderSource(sVertex, vertexSource);
-            try
-            {
+            try {
                 GL.CompileShader(sVertex);
-            }
-            catch (InvalidOperationException ioe)
-            {
+            } catch (InvalidOperationException ioe) {
                 Console.WriteLine(ioe.Message);
                 CheckCompileErrors(sVertex, "VERTEX");
             }
 
             sFragment = GL.CreateShader(ShaderType.FragmentShader);
             GL.ShaderSource(sFragment, fragmentSource);
-            try
-            {
+            try {
                 GL.CompileShader(sFragment);
-            }
-            catch (InvalidOperationException ioe)
-            {
+            } catch (InvalidOperationException ioe) {
                 Console.WriteLine(ioe.Message);
                 CheckCompileErrors(sFragment, "FRAGMENT");
             }
 
-            if (geometrySource != null)
-            {
+            if (geometrySource != null) {
                 gShader = GL.CreateShader(ShaderType.GeometryShader);
                 GL.ShaderSource((int)gShader, geometrySource);
                 GL.CompileShader((int)gShader);
@@ -57,8 +45,7 @@ namespace OpenGL_Breakout.Resources
             }
 
             ID = GL.CreateProgram();
-            try
-            {
+            try {
                 GL.AttachShader(ID, sVertex);
                 GL.AttachShader(ID, sFragment);
                 if (geometrySource != null)
@@ -69,190 +56,139 @@ namespace OpenGL_Breakout.Resources
                 GL.DeleteShader(sFragment);
                 if (geometrySource != null)
                     GL.DeleteShader((int)gShader);
-            }
-            catch (InvalidOperationException ioe)
-            {
+            } catch (InvalidOperationException ioe) {
                 Console.WriteLine(ioe.Message);
             }
         }
 
-        private void CheckCompileErrors(int obj, string type)
-        {
+        private void CheckCompileErrors(int obj, string type) {
             int success;
             string infoLog;
 
-            if (type != "PROGRAM")
-            {
+            if (type != "PROGRAM") {
                 GL.GetShader(ID, ShaderParameter.CompileStatus, out success);
-                if (success == 0)
-                {
+                if (success == 0) {
                     infoLog = GL.GetShaderInfoLog(obj);
                     Console.WriteLine("ERROR: Compile-time error: Type: {0} Handle: {1}", type, obj);
                     Console.WriteLine("{0}", infoLog);
                     Console.WriteLine("-----------------------------------------------------------------------");
 
                     ErrorCode error = GL.GetError();
-                    if (error == ErrorCode.InvalidValue || error == ErrorCode.InvalidOperation)
-                    {
+                    if (error == ErrorCode.InvalidValue || error == ErrorCode.InvalidOperation) {
                         throw new InvalidOperationException(infoLog);
                     }
                 }
-            }
-            else
-            {
+            } else {
                 GL.GetProgram(ID, GetProgramParameterName.LinkStatus, out success);
-                if (success == 0)
-                {
+                if (success == 0) {
                     infoLog = GL.GetProgramInfoLog(obj);
                     Console.WriteLine("ERROR: Link-time error: Type: {0}", type);
                     Console.WriteLine("{0}", infoLog);
                     Console.WriteLine("-----------------------------------------------------------------------");
 
                     ErrorCode error = GL.GetError();
-                    if (error == ErrorCode.InvalidValue || error == ErrorCode.InvalidOperation)
-                    {
+                    if (error == ErrorCode.InvalidValue || error == ErrorCode.InvalidOperation) {
                         throw new InvalidOperationException(infoLog);
                     }
                 }
             }
         }
 
-        public void SetFloat(string name, float value, bool useShader = false)
-        {
+        public void SetFloat(string name, float value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform1(GL.GetUniformLocation(ID, name), value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Float: " + e.Message);
             }
         }
 
-        public void SetInteger(string name, int value, bool useShader = false)
-        {
+        public void SetInteger(string name, int value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform1(GL.GetUniformLocation(ID, name), value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Integer: " + e.Message);
             }
         }
 
-        public void SetVector2f(string name, float x, float y, bool useShader = false)
-        {
+        public void SetVector2f(string name, float x, float y, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform2(GL.GetUniformLocation(ID, name), x, y);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector2(x,y): " + e.Message);
             }
         }
 
-        public void SetVector2(string name, Vector2 value, bool useShader = false)
-        {
+        public void SetVector2(string name, Vector2 value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform2(GL.GetUniformLocation(ID, name), value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector2(Vector2): " + e.Message);
             }
         }
 
-        public void SetVector3(string name, float x, float y, float z, bool useShader = false)
-        {
+        public void SetVector3(string name, float x, float y, float z, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform3(GL.GetUniformLocation(ID, name), x, y, z);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector3(x,y,z): " + e.Message);
             }
         }
 
-        public void SetVector3(string name, Vector3 value, bool useShader = false)
-        {
+        public void SetVector3(string name, Vector3 value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform3(GL.GetUniformLocation(ID, name), value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector3(Vector3): " + e.Message);
             }
         }
 
-        public void SetVector4(string name, float x, float y, float z, float w, bool useShader = false)
-        {
+        public void SetVector4(string name, float x, float y, float z, float w, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform4(GL.GetUniformLocation(ID, name), x, y, z, w);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector4(x,y,z,w): " + e.Message);
             }
         }
 
-        public void SetVector4(string name, Vector4 value, bool useShader = false)
-        {
+        public void SetVector4(string name, Vector4 value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.Uniform4(GL.GetUniformLocation(ID, name), value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Vector4(Vector4): " + e.Message);
             }
         }
 
-        public void SetMatrix4(string name, Matrix4 value, bool useShader = false)
-        {
+        public void SetMatrix4(string name, Matrix4 value, bool useShader = false) {
             if (useShader)
                 Use();
-            try
-            {
+            try {
                 GL.UniformMatrix4(GL.GetUniformLocation(ID, name), false, ref value);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine("Error Setting Matrix4: " + e.Message);
             }
         }
 
-        public virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                try
-                {
+        public virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                try {
                     GL.DeleteProgram(ID);
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Console.WriteLine("Error Deleting Shader Program: " + e.Message);
                 }
 
@@ -260,8 +196,7 @@ namespace OpenGL_Breakout.Resources
             }
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
